662efe6bcd9e53798fb918d792383847a377fd5e
Maryori Hilares <mhilaresa@unsa.edu.pe>
2020-08-02 18:35:51 -0500

    carteraclientes

---
+++ app/src/main/java/com/example/edward/carteraclientes/ActMain.java
@@ -0,0 +1,33 @@
+package com.example.edward.carteraclientes;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class ActMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it= new Intent( ActMain.this, ActNuevoCliente.class);
+                startActivity(it);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/edward/carteraclientes/ActNuevoCliente.java
@@ -0,0 +1,50 @@
+package com.example.edward.carteraclientes;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class ActNuevoCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_nuevo_cliente);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        }
+        @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+            MenuInflater inflater= getMenuInflater();
+            inflater.inflate(R.menu.menu_act_nuevo_cliente,menu);
+
+            return super.onCreateOptionsMenu(menu);
+        }
+
+
+    public  boolean onOptionsItemSeleccted(MenuItem item){
+        int id= item.getItemId();
+        switch (id){
+            case  R.id.action_ok:
+                Toast.makeText( this,  "Boton OK seleccionado", Toast.LENGTH_SHORT).show();
+                break;
+            case  R.id.action_cancelar:
+                Toast.makeText(this, "Boton Cancelar seleccionado", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return  super.onOptionsItemSelected(item);
+        }
+    }
